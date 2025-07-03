df6601fdd1d4bb714c785b829b6dd973c5ef7db0
Tuominiemi William <william.tuominiemi@aalto.ficd>
2025-07-03 14:07:26 +0300

    chessboard

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,34 @@ async fn main() {
 
         draw_edges();
 
+        draw_board();
+
         next_frame().await
+    }
+}
+
+fn draw_board() {
+    let block_size = screen_width() / 9.0;
+    let block_offset = screen_width() / 18.0;
+
+    for i in 0..8 {
+        for j in 0..8 {
+            let mut block_color: Color = WHITE;
+
+            if i % 2 == 1 && j % 2 == 0 {
+                block_color = BLACK;
+            } else if i % 2 == 0 && j % 2 == 1 {
+                block_color = BLACK;
+            }
+
+            draw_rectangle(
+                i as f32 * block_size + block_offset,
+                j as f32 * block_size + block_offset,
+                block_size,
+                block_size,
+                block_color,
+            );
+        }
     }
 }
 
@@ -27,11 +54,13 @@ fn draw_edges() {
     let characters: Vec<char> = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
     let numbers: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8];
 
+    let block_offset = screen_width() / 18.0;
+
     for n in numbers {
         draw_text(
             &n.to_string(),
-            0.0,
-            n as f32 * screen_height() / 8.0 - screen_height() / 8.0 / 2.0,
+            block_offset / 2.0,
+            n as f32 * screen_height() / 9.0 - screen_height() / 9.0 / 2.0 + block_offset,
             30.0,
             BLACK,
         );
@@ -40,8 +69,8 @@ fn draw_edges() {
     for c in 0..characters.len() {
         draw_text(
             &characters[c].to_string(),
-            c as f32 * screen_width() / 8.0 + screen_width() / 8.0 / 2.0,
-            screen_height(),
+            c as f32 * screen_width() / 9.0 + screen_width() / 9.0 / 2.0 + block_offset,
+            screen_height() - block_offset / 2.0,
             30.0,
             BLACK,
         );
